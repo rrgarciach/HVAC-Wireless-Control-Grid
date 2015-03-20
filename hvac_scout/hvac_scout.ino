79be9bdfadc49216991ad1173fada77b5245d3b0
Ruy Garcia <rrgarciach@gmail.com>
2015-03-19 23:25:55 -0600

    master: Added movement check and time period on Scout Device.

--- hvac_scout/hvac_scout.ino
+++ hvac_scout/hvac_scout.ino
@@ -15,6 +15,9 @@ bool booRelay = false;
 bool booPIR = false;
 
 int tempCurrent = 0;
+int timeCurrent = 0;
+int timeLastEvent = 0;
+int timeDelayThreshold = 60000; // 60 seconds.
 
 void setup() {
   Serial.begin(9600);
@@ -27,6 +30,16 @@ void setup() {
 void loop() {
   booPIR = digitalRead(pinPIR);
   tempCurrent = ( (5.0 * analogRead(pinLM35)*100.0)/1024.0 );
+  
+  // Check if movement:
+  if ( booPIR ) {
+    timeCurrent = millis();
+    timeThreshold = timeCurrent - timeLastEvent;
+  }
+  // If there's no movement after 60 seconds, send data to Master Device
+  if ( timeThreshold > timeDelayThreshold ) {
+    master.println("no_movement");
+  }
   
   master.println(tempCurrent);
 }
