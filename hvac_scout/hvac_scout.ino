e51b4f43742cfdce92d980ae6601cd4e257d86d6
Ruy Garcia <rrgarciach@gmail.com>
2015-03-28 13:17:10 -0600

    Enhanced data types and commented Serial print to increase free dynamic memory.

--- hvac_scout/hvac_scout.ino
+++ hvac_scout/hvac_scout.ino
@@ -4,12 +4,12 @@
 // Definition of digital pins:
 // slave Bluetooth socket to communicate with Master Device:
 // bluetooth module's TX goes to arduino's TX (in this case, pin 3).
-int pinBtRx = 2;
-int pinBtTx = 3;
-int pinRelay = 4;
-int pinPIR = 5;
-int pinIRLed = 13;
-int pinLM35 = 0;
+const uint8_t pinBtRx = 2;
+const uint8_t pinBtTx = 3;
+const uint8_t pinRelay = 4;
+const uint8_t pinPIR = 5;
+const uint8_t pinIRLed = 13;
+const uint8_t pinLM35 = 0;
 
 SoftwareSerial master(pinBtRx,pinBtTx);
 IRSignalSender irSender(pinIRLed);
@@ -17,19 +17,19 @@ IRSignalSender irSender(pinIRLed);
 bool booRelay = false;
 bool booPIR = false;
 
-int tempCurrent = 0;
-int timeCurrent = 0;
-int timeLastEvent = 0;
-int timeDelayThreshold = 60000; // 60 seconds.
-int timeThreshold;
+uint16_t tempCurrent = 0;
+uint16_t timeCurrent = 0;
+uint16_t timeLastEvent = 0;
+uint16_t timeDelayThreshold = 60000; // 60 seconds.
+uint16_t timeThreshold;
 
 void setup() {
-  Serial.begin(9600);
+//  Serial.begin(9600);
   master.begin(38400);
   pinMode(pinRelay, OUTPUT);
   pinMode(pinPIR, INPUT);
   pinMode(pinLM35, INPUT);
-  Serial.println("Beginning...");
+//  Serial.println("Beginning...");
 }
 
 void loop() {
