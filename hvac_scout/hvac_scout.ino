a8c68688c33ba54b5a8308982c400c0160005c9d
Ruy Garcia <rrgarciach@gmail.com>
2015-03-31 16:37:21 -0600

    Set pin variables as compile directives and added movement time delay detection.

--- hvac_scout/hvac_scout.ino
+++ hvac_scout/hvac_scout.ino
@@ -4,51 +4,63 @@
 // Definition of digital pins:
 // slave Bluetooth socket to communicate with Master Device:
 // bluetooth module's TX goes to arduino's TX (in this case, pin 3).
-const uint8_t pinBtRx = 2;
-const uint8_t pinBtTx = 3;
-const uint8_t pinRelay = 4;
-const uint8_t pinPIR = 5;
-const uint8_t pinIRLed = 13;
-const uint8_t pinLM35 = 0;
+#define pinBtRx 2
+#define pinBtTx 3
+#define pinPIR 4
+#define pinIRLed 13
+#define pinLM35 0
 
-SoftwareSerial master(pinBtRx,pinBtTx);
+SoftwareSerial master(pinBtRx, pinBtTx);
 IRSignalSender irSender(pinIRLed);
 
-bool booRelay = false;
 bool booPIR = false;
+bool booQuietZone = false;
 
 uint16_t tempCurrent = 0;
-uint16_t timeCurrent = 0;
+
 uint16_t timeLastEvent = 0;
-uint16_t timeDelayThreshold = 60000; // 60 seconds.
+uint16_t timeDelayThreshold = 10000; // 60 seconds.
 uint16_t timeThreshold;
 
 void setup() {
-//  Serial.begin(9600);
+  Serial.begin(9600);
   master.begin(38400);
-  pinMode(pinRelay, OUTPUT);
   pinMode(pinPIR, INPUT);
   pinMode(pinLM35, INPUT);
-//  Serial.println("Beginning...");
+  Serial.println("Beginning...");
 }
 
 void loop() {
-  /*
-  booPIR = digitalRead(pinPIR);
-  tempCurrent = ( (5.0 * analogRead(pinLM35)*100.0)/1024.0 );
   
+  tempCurrent = ( (5.0 * analogRead(pinLM35)*100.0)/1024.0 );
+
   // Check if movement:
+  booPIR = digitalRead(pinPIR);
+  Serial.println(booPIR);
   if ( booPIR ) {
-    timeCurrent = millis();
-    timeThreshold = timeCurrent - timeLastEvent;
+    timeLastEvent = millis();
   }
+  timeThreshold = millis() - timeLastEvent;
+  if (timeThreshold > timeDelayThreshold) {
+    booQuietZone = true;
+    Serial.println("movement");
+  } else {
+    booQuietZone = false;
+    Serial.println("quiet");
+  }
+  
   // If there's no movement after 60 seconds, send data to Master Device
   if ( timeThreshold > timeDelayThreshold ) {
     master.println("no_movement");
   }
-  
+
   master.println(tempCurrent);
-  */
-  irSender.sendCommand(1);
-  delay(3000);
+  // TURN ON command:
+//  irSender.sendCommand(1);
+//  delay(3000);
 }
+
+/*
+0 error
+1 
+*/
